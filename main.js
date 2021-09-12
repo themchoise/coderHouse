113fcb7f41acb0b1ae84b2a3debdc6477cdd86eb
themchoise <86273107+themchoise@users.noreply.github.com>
2021-09-11 23:33:45 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,118 @@
+
+hljs.initHighlightingOnLoad();
+
+const fnClaseCuatroComp = () => {
+
+    window.location.href='#preCode4Comp';
+
+    setTimeout(() => {
+        
+
+        let presupuestoInicial  =  parseInt(prompt('Calculo de Presupuesto para Empleados\nIngrese el monto del Presupuesto Inicial'));
+        let empleados = parseInt(prompt('Ingrese la cantidad de empleados'));
+        let salario = parseInt(prompt('Ingrese el salario del empleado'));
+        let gastoTotalEmpleados=0;
+        let impuestosEmpelado=0.20;
+        let balance=true;
+    
+            const validacionBalance = () => 
+        {
+            if ((presupuestoInicial-(salario*empleados))<0)
+            balance=false;
+            return;
+        }
+    
+        validacionBalance()
+    
+        const fnGastoEmpleados = (sueldo, empleados) =>  {
+            let resultado=(sueldo*empleados);
+            gastoTotalEmpleados=resultado;
+            return resultado;
+        }
+    
+        const fnValidarGasto= (gastoTotalEmpleados, presupuestoInicial) =>  {
+            let diferencia=0
+    
+            let resultado=(presupuestoInicial-gastoTotalEmpleados)
+            if (resultado < 0){
+                
+                diferencia=(gastoTotalEmpleados-presupuestoInicial)
+                  return ( 'Erronea!, El gasto de empleados excede su presupuesto por $' + diferencia +'\nNecesita un presupuesto de: $' +(presupuestoInicial+diferencia)  )
+            }else{
+                return ('Exitosa!, Su presupuesto alcanza. \nLuego de pagar el sueldo inicial su presupuesto restante es de $' +resultado )
+            }
+    
+        }
+    
+        const fnCalculoImpuestos=(sueldo, empleados) => {
+            let totalImpuestoEmpleado=(sueldo*empleados);
+            totalImpuestoEmpleado=(totalImpuestoEmpleado*impuestosEmpelado);
+            return totalImpuestoEmpleado ;
+        }
+    
+    
+        if (presupuestoInicial>0 && empleados>0 && salario >0)
+        {
+            console.log(balance + 'fin')
+            if  (balance){
+                 alert(
+                    'Su presupuesto fue de: $' + presupuestoInicial
+                    +'\nEl Gasto Total de Empleados es: $'+ fnGastoEmpleados(salario, empleados)
+                    +'\nSu validacion de gasto fue: ' + fnValidarGasto(gastoTotalEmpleados, presupuestoInicial)
+                    +'\nEl impuesto a pagar por empleado es de $' +  fnCalculoImpuestos(salario,empleados)
+                    +'\nEl impuesto total de empleados es: $'+(fnCalculoImpuestos(salario,empleados)*empleados)
+                 )
+            }
+            else{           
+            alert(
+                'Su presupuesto fue de: $' + presupuestoInicial
+                +'\nEl Gasto Total de Empleados es: $'+ fnGastoEmpleados(salario, empleados)
+                +'\nSu validacion de gasto fue: ' + fnValidarGasto(gastoTotalEmpleados, presupuestoInicial)
+             )
+               }
+            
+        }
+        else alert('Presupuesto, Empleados o Salario no puede ser 0')
+
+
+    }, 1500);
+
+   
+
+}
+
+
+const fnClaseCuatro = () => {
+
+    window.location.href='#preCode4';
+
+    setTimeout(() => {
+        let montoProducto = parseInt(prompt('Ingrese el monto del Producto'));
+
+    if (montoProducto > 0 )
+    {
+    let cuotas = parseInt(prompt('Ingrese la cantidad de Cuotas'));
+    const caculoCuotas = (monto, cuotas)  => {
+        let valorCuota = parseFloat((monto/cuotas));
+        return valorCuota;
+    }
+    
+    alert('El valor de cada cuota es $' + caculoCuotas(montoProducto,cuotas));
+    }
+    else{
+        alert('Monto debe ser > a $0 o un valor numerico');
+    }
+    }, 1500);
+
+    
+}
+
+
+
+
+
+
+
 const fnClaseTres = () => {
 
     let fin = 3;
@@ -84,17 +199,5 @@ const fnClase = () => {
     console.log(`Hola ${nombre} dentro de 10 años vas a tener ${(parseInt(edad)) + 10}`);
     console.log(`Hola ${nombre} naciste en el año ${añoActual - (parseInt(edad))}`)
 };
-
-
-//let numero = prompt('Ingrese un numero')
-
-/*
-numero = parseInt(numero)
-
-for (let i = 0; i < numero; i++) {
-    console.log('Hola')
-}
-*/
-
 
 
